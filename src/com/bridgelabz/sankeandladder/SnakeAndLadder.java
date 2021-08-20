ab833c6358a7dfc4409ea8a9cd0ee2395c7a06c5
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 19:35:02 +0530

    ADDED: uc7-two player game

--- src/com/bridgelabz/sankeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/sankeandladder/SnakeAndLadder.java
@@ -4,13 +4,21 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		
-		int pos = 0;
-		int total_dice_rolls = 0;
+		int pos1 = 0, pos2=0,pos,iteration=0;
 		
-		while(pos<100) {
+		while(pos1<100 && pos2<100) {
+			
+			if(iteration%2==0) {
+				pos=pos1;
+				System.out.println("Player 1's turn who is at: "+pos1);
+			}
+			else {
+				pos=pos2;
+				System.out.println("Player 2's turn who is at: "+pos2);
+			}
+			iteration++;
 			
 			int dice = (int) Math.floor(Math.random()*100)%6+1;
-			total_dice_rolls++;
 			int option = (int) Math.floor(Math.random()*100)%3;
 			
 			switch(option) {
@@ -35,13 +43,23 @@ public class SnakeAndLadder {
 			
 			}
 			System.out.println("Dice Rolled : "+dice);
-			System.out.println("Current Position : "+pos);
-			System.out.println(total_dice_rolls+"th DICE ROLL...");
 			
+			if((iteration-1)%2==0)
+				pos1=pos;
+			else
+				pos2=pos;
+			
+			System.out.println("Current Position of player 1: "+pos1);
+			System.out.println("Current Position of player 2: "+pos2+"\n");
+			
+			if(option==1)
+				iteration--;
 		}
 		
-		System.out.println("WINNER!");
-		System.out.println(" TOOK "+total_dice_rolls+" DICE ROLLS TO WIN!");
+		if(pos1==100)
+			System.out.println("PLAYER 1 is WINNER!");
+		else
+			System.out.println("PLAYER 2 is WINNER!");
 		
 	}
 
