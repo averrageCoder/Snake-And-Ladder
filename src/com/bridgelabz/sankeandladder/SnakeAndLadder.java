b76b1bffe26e80d0cbbfac6055a1b5c26c04a7c8
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 18:48:41 +0530

    ADDED: uc3 - option check

--- src/com/bridgelabz/sankeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/sankeandladder/SnakeAndLadder.java
@@ -6,8 +6,26 @@ public class SnakeAndLadder {
 		
 		int pos = 0;
 		int dice = (int) Math.floor(Math.random()*100)%6+1;
+		int option = (int) Math.floor(Math.random()*100)%3;
+		
+		switch(option) {
+		
+		case 0: 
+			System.out.println("NO PLAY!");
+			break;
+			
+		case 1:
+			System.out.println("HURRAY! - LADDER!");
+			pos+=dice;
+			break;
+			
+		case 2:
+			System.out.println("OOPS! - SNAKE!");
+			pos-=dice;
+			break;
+		
+		}
 		System.out.println("Dice Rolled : "+dice);
-		pos+=dice;
 		System.out.println("Current Position : "+pos);
 
 	}
