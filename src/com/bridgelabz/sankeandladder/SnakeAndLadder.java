e5af4684004d8a4fc3fa4b64a4e88230e98c6a1a
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 18:30:34 +0530

    uc1-initial setup

---
+++ src/com/bridgelabz/sankeandladder/SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.sankeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		int pos = 0;
+		
+		System.out.println("Current Position : "+pos);
+
+	}
+
+}
