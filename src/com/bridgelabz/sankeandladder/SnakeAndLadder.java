bc3d24be7f57ed90376cb3e27b33a07fdb933a8b
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 18:58:26 +0530

    ADDED: uc4 - till user reaches 100

--- src/com/bridgelabz/sankeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/sankeandladder/SnakeAndLadder.java
@@ -5,28 +5,37 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		
 		int pos = 0;
-		int dice = (int) Math.floor(Math.random()*100)%6+1;
-		int option = (int) Math.floor(Math.random()*100)%3;
 		
-		switch(option) {
-		
-		case 0: 
-			System.out.println("NO PLAY!");
-			break;
+		while(pos<=100) {
 			
-		case 1:
-			System.out.println("HURRAY! - LADDER!");
-			pos+=dice;
-			break;
+			int dice = (int) Math.floor(Math.random()*100)%6+1;
+			int option = (int) Math.floor(Math.random()*100)%3;
 			
-		case 2:
-			System.out.println("OOPS! - SNAKE!");
-			pos-=dice;
-			break;
-		
+			switch(option) {
+			
+			case 0: 
+				System.out.println("NO PLAY!");
+				break;
+				
+			case 1:
+				System.out.println("HURRAY! - LADDER!");
+				pos+=dice;
+				
+				break;
+				
+			case 2:
+				System.out.println("OOPS! - SNAKE!");
+				pos-=dice;
+				if(pos<0)
+					pos=0;
+				break;
+			
+			}
+			System.out.println("Dice Rolled : "+dice);
+			System.out.println("Current Position : "+pos);
+			
 		}
-		System.out.println("Dice Rolled : "+dice);
-		System.out.println("Current Position : "+pos);
+		
 
 	}
 
